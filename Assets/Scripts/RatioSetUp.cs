99b481337f4960e2ac887c1eaea5237173c3ad9e
AMeghnad <anshumanmeghnad@gmail.com>
2018-06-05 09:43:28 +1000

    Added resolution setup

---
+++ Assets/Scripts/RatioSetUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatioSetUp : MonoBehaviour
+{
+    public int width, height;
+
+    void Awake()
+    {
+        if (width == 0 || height == 0)
+        {
+            width = 800;
+            height = 1280;
+        }
+        Screen.SetResolution(width, height, false);
+    }
+}
